e0468b1950a5ef4cc82fc31d0fa29dd3b73f1037
Rostam <a.rostami@uni-jena.de>
2015-01-06 11:43:50 +0100

    the edge degree report added

---
+++ src/graphtea/extensions/reports/basicreports/EdgeDegree.java
@@ -0,0 +1,50 @@
+// GraphTea Project: http://github.com/graphtheorysoftware/GraphTea
+// Copyright (C) 2012 Graph Theory Software Foundation: http://GraphTheorySoftware.com
+// Copyright (C) 2008 Mathematical Science Department of Sharif University of Technology
+// Distributed under the terms of the GNU General Public License (GPL): http://www.gnu.org/licenses/
+package graphtea.extensions.reports.basicreports;
+
+import graphtea.graph.graph.Edge;
+import graphtea.platform.lang.CommandAttitude;
+import graphtea.plugins.main.GraphData;
+import graphtea.plugins.reports.extension.GraphReportExtension;
+
+/**
+ * @author azin azadi
+
+ */
+
+
+@CommandAttitude(name = "edge_degree", abbreviation = "_edegree")
+public class EdgeDegree implements GraphReportExtension<Integer> {
+    public String getName() {
+        return "Edge Degree";
+    }
+
+    public String getDescription() {
+        return "For the edge u~v the edge degree is d(u)+d(v)-2.";
+    }
+
+    public Integer calculate(GraphData gd) {
+        Edge ee = null;
+        for(Edge e : gd.getGraph().getEdges()) {
+            if(e.isSelected()) {
+                ee = e;
+                break;
+            }
+        }
+        if(ee != null) {
+            return gd.getGraph().getDegree(ee.source) +
+                    gd.getGraph().getDegree(ee.target) -
+                    2;
+        }
+
+        return 0;
+    }
+
+	@Override
+	public String getCategory() {
+		// TODO Auto-generated method stub
+		return "Property";
+	}
+}
