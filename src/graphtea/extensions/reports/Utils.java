9b552b96c1f1a364a69c9eee2ca5aa44e35b7feb
Rostam <a.rostami@uni-jena.de>
2015-09-30 13:01:50 +0200

    correcting min nonpendent degreee

---
+++ src/graphtea/extensions/reports/Utils.java
@@ -0,0 +1,29 @@
+package graphtea.extensions.reports;
+
+import graphtea.graph.graph.GraphModel;
+import graphtea.plugins.main.core.AlgorithmUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by rostam on 30.09.15.
+ */
+public class Utils {
+    // get kth minimum degree
+    public static double getMinNonPendentDegree(GraphModel g) {
+        ArrayList<Integer> al = AlgorithmUtils.getDegreesList(g);
+        Collections.sort(al);
+        if(al.contains(1)) {
+            for(int i=0;i<al.size();i++) {
+                if(al.get(i)!=1) {
+                    return al.get(i);
+                }
+            }
+        }
+
+        return al.get(0);
+    }
+
+
+}
