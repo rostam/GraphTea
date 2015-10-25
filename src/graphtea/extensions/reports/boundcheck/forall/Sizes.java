c9ba044471887463f4c646d7f7ec656caa0fdb0b
ali <ali@c3e.de>
2015-10-25 19:09:51 +0100

    adding the sizes of trees

--- src/graphtea/extensions/reports/boundcheck/forall/Sizes.java
+++ src/graphtea/extensions/reports/boundcheck/forall/Sizes.java
@@ -21,24 +21,23 @@ public class Sizes {
 
         sizes.put("tree2",1);
         sizes.put("tree3",1);
-        sizes.put("tree4",1);
-        sizes.put("tree5",1);
-        sizes.put("tree6",1);
-        sizes.put("tree7",1);
-        sizes.put("tree8",1);
-        sizes.put("tree9",1);
-        sizes.put("tree10",1);
-        sizes.put("tree11",1);
+        sizes.put("tree4",2);
+        sizes.put("tree5",3);
+        sizes.put("tree6",6);
+        sizes.put("tree7",11);
+        sizes.put("tree8",23);
+        sizes.put("tree9",47);
+        sizes.put("tree10",106);
+        sizes.put("tree11",235);
 
-
-        sizes.put("tree12",1);
-        sizes.put("tree13",1);
-        sizes.put("tree14",1);
-        sizes.put("tree15",1);
-        sizes.put("tree16",1);
-        sizes.put("tree17",1);
-        sizes.put("tree18",1);
-        sizes.put("tree19",1);
-        sizes.put("tree20",1);
+        sizes.put("tree12",551);
+        sizes.put("tree13",1301);
+        sizes.put("tree14",3159);
+        sizes.put("tree15",7741);
+        sizes.put("tree16",19320);
+        sizes.put("tree17",48629);
+        sizes.put("tree18",123867);
+        sizes.put("tree19",317955);
+        sizes.put("tree20",823065);
     }
 }
