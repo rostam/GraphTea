b137d3fae29da79ee8cd8666f5ac0a9f1001a7ad
ali <ali@c3e.de>
2015-12-18 23:26:07 +0100

    adding static save object

--- src/graphtea/extensions/io/GraphSaveObject.java
+++ src/graphtea/extensions/io/GraphSaveObject.java
@@ -4,7 +4,7 @@ import graphtea.graph.graph.Edge;
 import graphtea.graph.graph.GraphModel;
 import graphtea.graph.graph.Vertex;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.Vector;
 
 /**
@@ -32,5 +32,32 @@ public class GraphSaveObject implements Serializable {
         }
         g.setDirected(directed);
         return g;
+    }
+
+    public static byte[] getBytesOfGraph(GraphModel g) {
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        try {
+            ObjectOutputStream oop = new ObjectOutputStream(bout);
+            oop.writeObject(new GraphSaveObject(g));
+            oop.flush();
+            oop.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static GraphModel getGraphFromBytes(byte[] b) {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(
+                    new ByteArrayInputStream(b));
+            GraphSaveObject gso = (GraphSaveObject) ois.readObject();
+            return gso.getG();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
