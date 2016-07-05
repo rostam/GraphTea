4ca512793745e497704017c5220521843e8b740a
Ali <a.rostami@uni-jena.de>
2016-07-05 13:29:25 +0200

    small change

--- src/graphtea/extensions/reports/coloring/fillinaware/FillinAwareColoring.java
+++ src/graphtea/extensions/reports/coloring/fillinaware/FillinAwareColoring.java
@@ -7,127 +7,134 @@ import graphtea.graph.graph.RenderTable;
 import graphtea.platform.parameter.Parameter;
 import graphtea.platform.parameter.Parametrizable;
 import graphtea.plugins.reports.extension.GraphReportExtension;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Vector;
 
-public class FillinAwareColoring implements GraphReportExtension,Parametrizable {
-  //  @Parameter(name = "Matrix File", description = "")
-//  public String file = "abb313.mtx";
-  @Parameter(name = "k from ILU(k)")
-  public int el = 2;
+public class FillinAwareColoring implements GraphReportExtension, Parametrizable {
+    @Parameter(name = "k from ILU(k)")
+    public int el = 2;
 
-  public static String getPathOfMats() {
-    String cur = null;
-    try {
-      cur = new java.io.File(".").getCanonicalPath();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    @Parameter(name = "Block sizes")
+    public String blSizes = "4,10";
 
-    if (System.getProperty("os.name").contains("Win")) {
-      cur = cur + "\\matrices\\";
-    } else {
-      cur = cur + "/matrices/";
-    }
-
-    return cur;
-  }
-
-  @Override
-  public Object calculate(GraphModel g) {
-    RenderTable ret = new RenderTable(1);
-    Vector<String> titles = new Vector<>();
-    titles.add("Matrix");
-    titles.add("Fillin");
-    titles.add("Rows");
-    titles.add("NNZ");
-    titles.add("F");
-    titles.add("C");
-    titles.add("add");
-    ret.setTitles(titles);
-    File dir = new File(getPathOfMats());
-    File[] directoryListing = dir.listFiles();
-    if (directoryListing != null) {
-      for (File f : directoryListing) {
+    public static String getPathOfMats() {
+        String cur = null;
         try {
-          System.out.println("file:" + f.getAbsolutePath());
-          Matrix m = MM.loadMatrixFromSPARSE(f);
-          SpMat sm = new SpMat(m);
-          //int sizes[] = {1,10,sm.rows()/32,sm.rows()/8};
-          int sizes[] = {10,sm.rows()/32,sm.rows()/8};
-          for(int i=0;i<sizes.length;i++) {
-            generateResults(ret, f, sm, "Nat", "Nat", sizes[i]);
-            generateResults(ret, f, sm, "MinDeg", "MinDeg", sizes[i]);
-            generateResults(ret, f, sm, "Metis", "Metis", sizes[i]);
-            generateResults(ret, f, sm, "Nat", "Nat", sizes[i]);
-            generateResults(ret, f, sm, "Metis", "MaxDeg", sizes[i]);
-            generateResults(ret, f, sm, "Nat", "MaxDeg", sizes[i]);
-          }
+            cur = new java.io.File(".").getCanonicalPath();
         } catch (IOException e) {
-          e.printStackTrace();
+            e.printStackTrace();
         }
-      }
+
+        if (System.getProperty("os.name").contains("Win")) {
+            cur = cur + "\\matrices\\";
+        } else {
+            cur = cur + "/matrices/";
+        }
+
+        return cur;
     }
-    //MatrixHeatMap hm = new MatrixHeatMap(mmDiv8);
-    return ret;
-  }
 
-  private void generateResults(RenderTable ret, File f, SpMat  sm,
-                               String ordILU, String ordCol,int k) {
-    ColInf cinf = computeOneMatrix(sm,ordILU,ordCol,k);
-    Vector<Object> results = new Vector<>();
-    results.add(f.getName()+","+ordILU+","+ordCol+",k="+k);
-    results.add(cinf.F*1.0);
-    results.addAll(cinf.getVec());
-    ret.add(results);
-  }
+    @Override
+    public Object calculate(GraphModel g) {
+        RenderTable ret = new RenderTable(1);
+        Vector<String> titles = new Vector<>();
+        titles.add("Matrix");
+        titles.add("Fillin");
+        titles.add("Rows");
+        titles.add("NNZ");
+        titles.add("F");
+        titles.add("C");
+        titles.add("add");
+        ret.setTitles(titles);
+        File dir = new File(getPathOfMats());
+        File[] directoryListing = dir.listFiles();
+        if (directoryListing != null) {
+            for (File f : directoryListing) {
+                try {
+                    System.out.println("file:" + f.getAbsolutePath());
+                    Matrix m = MM.loadMatrixFromSPARSE(f);
+                    SpMat sm = new SpMat(m);
 
-  public ColInf computeOneMatrix(SpMat mm, String ordILU,String ordCol,int k) {
-    SpMat block = mm.sparsify(k);
-    GraphModel gILU = Helper.getGraphOfILU(block);
-    GraphModel gCol = Helper.getGraphOfColoring(block);
-    Vector<Integer> orderILU = HCol.ordering(gCol, ordILU, block);
-    Vector<Integer> orderCol = HCol.ordering(gCol, ordCol, block);
-    SpMat Fmat = Helper.SILU(gILU, el, orderILU, block);
-    int F=Fmat.nnz()-block.nnz();
-    int C= HCol.colorRest(gCol, orderCol);
-    SpMat Pmat = Helper.getPotReqEdges(gCol, mm, block);
-    int P = Pmat.nnz();
-    SpMat addM = Helper.getAddReqEdges(gILU, mm, block, Pmat, orderILU);
-    int add = addM.nnz();
-    //saveMatriices(block,Fmat,addM,ordILU,ordCol);
-    return new ColInf(block.rows(),block.nnz(),F,C,P,add);
-  }
-
-  public void saveMatriices(SpMat block, SpMat fillin,
-                            SpMat add, String ordILU,String ordCol) {
-    try {
-      MM.saveMtxFormat(new File("mats/m10" + ordILU + ordCol+ ".mtx"), block);
-      MM.saveMtxFormat(new File("mats/F10" + ordILU + ordCol+ ".mtx"),fillin);
-      MM.saveMtxFormat(new File("mats/add10" + ordILU + ordCol+ ".mtx"),add);
-    } catch (IOException e) {
-      e.printStackTrace();
+                    //int sizes[] = {1,10,sm.rows()/32,sm.rows()/8};
+                    String[] tmp = blSizes.split(",");
+                    int sizes[] = new int[tmp.length];
+                    for (int i = 0; i < tmp.length; i++) {
+                        sizes[i] = Integer.parseInt(tmp[i]);
+                    }
+                    //int sizes[] = blSizes.split(",");//{4,10, sm.rows() / 32, sm.rows() / 8};
+                    for (int i = 0; i < sizes.length; i++) {
+                        generateResults(ret, f, sm, "Nat", "Nat", sizes[i]);
+                        generateResults(ret, f, sm, "MinDeg", "MinDeg", sizes[i]);
+                        //generateResults(ret, f, sm, "Metis", "Metis", sizes[i]);
+                        generateResults(ret, f, sm, "Metis", "MaxDeg", sizes[i]);
+                        //generateResults(ret, f, sm, "Nat", "MaxDeg", sizes[i]);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        //MatrixHeatMap hm = new MatrixHeatMap(mmDiv8);
+        return ret;
     }
-  }
 
-  @Override
-  public String getCategory() {
-    return "Bipartite Coloring";
-  }
+    private void generateResults(RenderTable ret, File f, SpMat sm,
+                                 String ordILU, String ordCol, int k) {
+        ColInf cinf = computeOneMatrix(sm, ordILU, ordCol, k);
+        Vector<Object> results = new Vector<>();
+        results.add(f.getName() + "," + ordILU + "," + ordCol + ",k=" + k);
+        results.add(cinf.F * 1.0);
+        results.addAll(cinf.getVec());
+        ret.add(results);
+    }
 
-  @Override
-  public String getName() {
-    return "Fillin-Aware Graph Coloring ";
-  }
+    public ColInf computeOneMatrix(SpMat mm, String ordILU, String ordCol, int k) {
+        SpMat block = mm.sparsify(k);
+        GraphModel gILU = Helper.getGraphOfILU(block);
+        GraphModel gCol = Helper.getGraphOfColoring(block);
+        Vector<Integer> orderILU = HCol.ordering(gCol, ordILU, block);
+        Vector<Integer> orderCol = HCol.ordering(gCol, ordCol, block);
+        SpMat Fmat = Helper.SILU(gILU, el, orderILU, block);
+        int F = Fmat.nnz() - block.nnz();
+        int C = HCol.colorRest(gCol, orderCol);
+        SpMat Pmat = Helper.getPotReqEdges(gCol, mm, block);
+        int P = Pmat.nnz();
+        SpMat addM = Helper.getAddReqEdges(gILU, mm, block, Pmat, orderILU);
+        int add = addM.nnz();
+        //saveMatriices(block,Fmat,addM,ordILU,ordCol);
+        return new ColInf(block.rows(), block.nnz(), F, C, P, add);
+    }
 
-  @Override
-  public String getDescription() {
-    return "Bipartite Graph Coloring";
-  }
+    public void saveMatriices(SpMat block, SpMat fillin,
+                              SpMat add, String ordILU, String ordCol) {
+        try {
+            MM.saveMtxFormat(new File("mats/m10" + ordILU + ordCol + ".mtx"), block);
+            MM.saveMtxFormat(new File("mats/F10" + ordILU + ordCol + ".mtx"), fillin);
+            MM.saveMtxFormat(new File("mats/add10" + ordILU + ordCol + ".mtx"), add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-  @Override
-  public String checkParameters() {
-    return null;
-  }
+    @Override
+    public String getCategory() {
+        return "Bipartite Coloring";
+    }
+
+    @Override
+    public String getName() {
+        return "Fillin-Aware Graph Coloring ";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Bipartite Graph Coloring";
+    }
+
+    @Override
+    public String checkParameters() {
+        return null;
+    }
 }
--- src/graphtea/extensions/reports/coloring/fillinaware/SpMat.java
+++ src/graphtea/extensions/reports/coloring/fillinaware/SpMat.java
@@ -9,25 +9,26 @@ import java.util.Vector;
  * Created by rostam on 21.01.16.
  *
  */
-public class SpMat extends Vector<HashSet<Integer>>{
-    private int rows,cols;
+public class SpMat extends Vector<HashSet<Integer>> {
+    private int rows, cols;
+
     public SpMat(int rows, int cols) {
-        this.rows=rows;
-        this.cols=cols;
+        this.rows = rows;
+        this.cols = cols;
         for (int i = 0; i < rows; i++) {
             this.add(new HashSet<Integer>());
         }
     }
 
     public SpMat(Matrix m) {
-        this.rows=m.getRowDimension();
-        this.cols=m.getColumnDimension();
+        this.rows = m.getRowDimension();
+        this.cols = m.getColumnDimension();
         for (int i = 0; i < rows; i++) {
             this.add(new HashSet<Integer>());
         }
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if(m.get(i, j) != 0) {
+                if (m.get(i, j) != 0) {
                     this.get(i).add(j);
                 }
             }
@@ -43,10 +44,10 @@ public class SpMat extends Vector<HashSet<Integer>>{
     }
 
     public SpMat copy() {
-        SpMat cp = new SpMat(rows,cols);
-        for(int i=0;i<rows;i++) {
+        SpMat cp = new SpMat(rows, cols);
+        for (int i = 0; i < rows; i++) {
             cp.add(new HashSet<Integer>());
-            for(int j : this.get(i)) {
+            for (int j : this.get(i)) {
                 cp.get(i).add(j);
             }
         }
@@ -55,15 +56,15 @@ public class SpMat extends Vector<HashSet<Integer>>{
 
     public int nnz() {
         int ret = 0;
-        for(int i=0;i<rows;i++) {
-            ret+=this.get(i).size();
+        for (int i = 0; i < rows; i++) {
+            ret += this.get(i).size();
         }
         return ret;
     }
 
 
-    public void set(int i,int j) {
-        if(!this.get(i).contains(j)) {
+    public void set(int i, int j) {
+        if (!this.get(i).contains(j)) {
             this.get(i).add(j);
         }
     }
@@ -73,24 +74,38 @@ public class SpMat extends Vector<HashSet<Integer>>{
     }
 
     public SpMat sparsify(int k) {
-        SpMat newMM = new SpMat(this.rows,this.cols);
-        for (int d = 0; d < ((rows / k) - 1) * k; d = d + k) {
-            for (int ii = 0; ii < k; ii++) {
-                for (int jj = 0; jj < k; jj++) {
-                    if ((d + ii) < rows && (d + jj) < cols) {
-                        if(this.contains(d + ii, d + jj)) {
-                            newMM.set(d+ii,d+jj);
+        SpMat newMM = new SpMat(this.rows, this.cols);
+        if(rows % k != 0) {
+            for (int d = 0; d < ((rows / k) - 1) * k; d = d + k) {
+                for (int ii = 0; ii < k; ii++) {
+                    for (int jj = 0; jj < k; jj++) {
+                        if ((d + ii) < rows && (d + jj) < cols) {
+                            if (this.contains(d + ii, d + jj)) {
+                                newMM.set(d + ii, d + jj);
+                            }
                         }
                     }
                 }
             }
-        }
 
-        int d1 = ((rows / k) - 1) * k;
-        for (int cnt1 = d1; cnt1 < rows; cnt1++) {
-            for (int cnt2 = d1; cnt2 < cols; cnt2++) {
-                if(this.contains(cnt1,cnt2)) {
-                    newMM.set(cnt1,cnt2);
+            int d1 = ((rows / k) - 1) * k;
+            for (int cnt1 = d1; cnt1 < rows; cnt1++) {
+                for (int cnt2 = d1; cnt2 < cols; cnt2++) {
+                    if (this.contains(cnt1, cnt2)) {
+                        newMM.set(cnt1, cnt2);
+                    }
+                }
+            }
+        } else {
+            for (int d = 0; d < rows; d = d + k) {
+                for (int ii = 0; ii <= k; ii++) {
+                    for (int jj = 0; jj <= k; jj++) {
+                        if ((d + ii) < rows && (d + jj) < cols) {
+                            if (this.contains(d + ii, d + jj)) {
+                                newMM.set(d + ii, d + jj);
+                            }
+                        }
+                    }
                 }
             }
         }
