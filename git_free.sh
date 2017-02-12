03b738ed4e06185e86278e4bc2cc4d04e08b0378
Ali <ali@c3e.de>
2017-02-12 12:01:44 +0100

    a script to make the graphtea git free (to send to suresh)

---
+++ git_free.sh
@@ -0,0 +1,8 @@
+mkdir ../GraphTeaGitFree
+cp -r * ../GraphTeaGitFree
+cd ../
+rm -rf GraphTeaGitFree/graphs/*
+rm -rf GraphTeaGitFree/.git*
+rm -rf GraphTeaGitFree/install4j*
+rm -rf GraphTeaGitFree/Coloring*
+rm GraphTeaGitFree/make_git_free.sh
