0bd12967e12b409fd639c1a887aaaef976fa2518
Ali Rostami <rostamiev@gmail.com>
2020-06-20 17:45:25 +0200

    adding graph strong product

---
+++ src/graphtea/extensions/actions/product/StrongProduct.java
@@ -0,0 +1,48 @@
+package graphtea.extensions.actions.product;
+
+import graphtea.graph.graph.GraphModel;
+import graphtea.graph.ui.GTabbedGraphPane;
+import graphtea.platform.parameter.Parameter;
+import graphtea.platform.parameter.Parametrizable;
+import graphtea.plugins.main.GraphData;
+import graphtea.plugins.main.extension.GraphActionExtension;
+
+/**
+ * Created by rostam on 10.07.15.
+ * @author M. Ali Rostami
+ */
+public class StrongProduct implements GraphActionExtension, Parametrizable {
+    @Parameter(name = "First Graph",description = "First Graph")
+    public String g0 = "G0";
+    @Parameter(name = "Second Graph",description = "Second Graph")
+    public String g1 = "G1";
+
+    @Override
+    public String getName() {
+        return "Strong Product";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Strong  Product";
+    }
+
+    @Override
+    public void action(GraphData graphData) {
+        GTabbedGraphPane gtp = graphData.getBlackboard().getData(GTabbedGraphPane.NAME);
+        GStrongProduct prod = new GStrongProduct();
+        GraphModel g= prod.multiply(gtp.getGraphs().get(g0), gtp.getGraphs().get(g1));
+        prod.setPositions(g);
+        graphData.core.showGraph(g);
+    }
+
+    @Override
+    public String checkParameters() {
+        return null;
+    }
+
+    @Override
+    public String getCategory() {
+        return "Products";
+    }
+}
