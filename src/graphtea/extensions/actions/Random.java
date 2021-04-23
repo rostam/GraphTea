aa07ca18cdd1a061b7ecd7e8ca85d0d551bf182d
Ali Rostami <rostami@axxessio.com>
2021-04-23 10:00:59 +0200

    locality lenses and random are moved to actions

--- src/graphtea/extensions/actions/Random.java
+++ src/graphtea/extensions/actions/Random.java
@@ -2,7 +2,7 @@
 // Copyright (C) 2012 Graph Theory Software Foundation: http://GraphTheorySoftware.com
 // Copyright (C) 2008 Mathematical Science Department of Sharif University of Technology
 // Distributed under the terms of the GNU General Public License (GPL): http://www.gnu.org/licenses/
-package graphtea.plugins.visualization.localsfvis;
+package graphtea.extensions.actions;
 
 import graphtea.graph.atributeset.GraphAttrSet;
 import graphtea.graph.graph.GPoint;
@@ -11,35 +11,33 @@ import graphtea.graph.graph.GraphModel;
 import graphtea.graph.graph.Vertex;
 import graphtea.platform.core.AbstractAction;
 import graphtea.platform.core.BlackBoard;
+import graphtea.plugins.main.GraphData;
+import graphtea.plugins.main.extension.GraphActionExtension;
 import graphtea.ui.UIUtils;
 
 /**
  * @author houshmand hasannia
  */
-public class Random extends AbstractAction {
-    private GraphModel g;
-
-    /**
-     * constructor
-     *
-     * @param bb the blackboard of the action
-     */
-    public Random(BlackBoard bb) {
-        super(bb);
-        String event = UIUtils.getUIEventKey("Random");
-        listen4Event(event);
+public class Random implements GraphActionExtension {
+    @Override
+    public String getName() {
+        return "Random Layout";
     }
 
-    /**
-     * like Action
-     *
-     * @param eventName The event name
-     * @param value The value
-     */
-    public void performAction(String eventName, Object value) {
-        g = blackboard.getData(GraphAttrSet.name);
-        n = g.getVerticesCount();
-        Vertex[] v = getVertices();
+    @Override
+    public String getDescription() {
+        return "Random Layout";
+    }
+
+    @Override
+    public String getCategory() {
+        return "Visualization";
+    }
+
+    @Override
+    public void action(GraphData graphData) {
+        GraphModel g = graphData.getGraph();
+        int n = g.getVerticesCount();
         GRect b = g.getZoomedBounds();
         int w = (int) b.w;
         int h = (int) b.h;
@@ -50,19 +48,8 @@ public class Random extends AbstractAction {
         for (int i = 0; i < n; i++) {
             double x = Math.random() * w;
             double y = Math.random() * h;
-            v[i].setLocation(new GPoint(x, y));
+            g.getVertex(i).setLocation(new GPoint(x, y));
         }
     }
-
-    private Vertex[] getVertices() {
-        Vertex[] ret = new Vertex[n];
-        int i = 0;
-        for (Vertex vm : g) {
-            ret[i++] = vm;
-        }
-        return ret;
-    }
-
-    private int n;
 }
 
